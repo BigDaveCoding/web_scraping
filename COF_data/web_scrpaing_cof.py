aed04f54b2ca02f16a14723552e4e0dccdc72350
BigDaveCoding <bigdavecoding@gmail.com>
2024-08-20 21:36:22 +0100

    created a soup.find to gether the info from table and the unique class for the table. Then taking the headers and storing them in a list and cleaning the list up.

--- COF_data/web_scrpaing_cof.py
+++ COF_data/web_scrpaing_cof.py
@@ -2,3 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import json
+
+costa_rica_2024_results_webpage = requests.get('https://allianceforcoffeeexcellence.org/costa-rica-2024/#coe-results')
+costa_rica_2024_soup = BeautifulSoup(costa_rica_2024_results_webpage.content, 'html.parser')
+
+costa_rica_dictionary = {}
+# cr_soup = costa_rica_2024_soup.find_all('div', '.mtr-cell-content')
+
+cr_table_washed = costa_rica_2024_soup.find('table', class_='experimental mtr-table mtr-tr-td')
+washed_headers = [header.get_text(strip=True) for header in cr_table_washed.find('tr')]
+clean_washed_headers = [header for header in washed_headers if header != '']
+print(clean_washed_headers)
+# print(cr_table_washed)
