9b84b1e63fa77f6ac55d60f16f221e2d7718473f
BigDaveCoding <bigdavecoding@gmail.com>
2024-08-20 22:02:46 +0100

    want to extract the data and store it in lists for corresponding keys in dictionary. Just need to figure out how to extract the data.

--- COF_data/web_scrpaing_cof.py
+++ COF_data/web_scrpaing_cof.py
@@ -9,8 +9,24 @@ costa_rica_2024_soup = BeautifulSoup(costa_rica_2024_results_webpage.content, 'h
 costa_rica_dictionary = {}
 # cr_soup = costa_rica_2024_soup.find_all('div', '.mtr-cell-content')
 
+# Find the table
 cr_table_washed = costa_rica_2024_soup.find('table', class_='experimental mtr-table mtr-tr-td')
+
+# Extract the headers and clean
 washed_headers = [header.get_text(strip=True) for header in cr_table_washed.find('tr')]
 clean_washed_headers = [header for header in washed_headers if header != '']
-print(clean_washed_headers)
-# print(cr_table_washed)
+# print(clean_washed_headers)
+
+washed_data = cr_table_washed.find('tbody').find_all('td')[1:]
+data = []
+
+for row in washed_data:
+    cells = row.find_all('td')
+    row_data = [cell.get_text() for cell in cells]
+    data.append(row_data)
+print(data)
+
+for i in range(0, len(clean_washed_headers)):
+    costa_rica_dictionary[clean_washed_headers[i]] = []
+
+# print(costa_rica_dictionary)
