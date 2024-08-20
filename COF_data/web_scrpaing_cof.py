93312d8c56d1aeb5f39914f5b58554defd611ec1
BigDaveCoding <bigdavecoding@gmail.com>
2024-08-20 22:03:30 +0100

    pressed save

--- COF_data/web_scrpaing_cof.py
+++ COF_data/web_scrpaing_cof.py
@@ -17,15 +17,6 @@ washed_headers = [header.get_text(strip=True) for header in cr_table_washed.find
 clean_washed_headers = [header for header in washed_headers if header != '']
 # print(clean_washed_headers)
 
-washed_data = cr_table_washed.find('tbody').find_all('td')[1:]
-data = []
-
-for row in washed_data:
-    cells = row.find_all('td')
-    row_data = [cell.get_text() for cell in cells]
-    data.append(row_data)
-print(data)
-
 for i in range(0, len(clean_washed_headers)):
     costa_rica_dictionary[clean_washed_headers[i]] = []
 
