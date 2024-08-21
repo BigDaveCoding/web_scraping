be7980d71b9339919447b3b189bf73071b1a6f90
BigDaveCoding <bigdavecoding@gmail.com>
2024-08-21 15:44:46 +0100

    created 2 functions. 1st function headers_for_dictionary takes a soup table and dictionary as parameters. It iterates through the first 'tr' element in the specified soup table and saves the text to a list. Then another list comprehension cleans the list. Iterates through length of list and adds each value as a key to the specified dictionary. then returns the clean list as I use this in the next function. 2nd function extracts the data from the coresponding soup table using the headers returned from 1st function and adds data to a list in the dictionary.

--- COF_data/web_scrpaing_cof.py
+++ COF_data/web_scrpaing_cof.py
@@ -7,24 +7,43 @@ costa_rica_2024_results_webpage = requests.get('https://allianceforcoffeeexcelle
 costa_rica_2024_soup = BeautifulSoup(costa_rica_2024_results_webpage.content, 'html.parser')
 
 costa_rica_dictionary = {}
-# cr_soup = costa_rica_2024_soup.find_all('div', '.mtr-cell-content')
 
 # Find the table
 cr_table_washed = costa_rica_2024_soup.find('table', class_='experimental mtr-table mtr-tr-td')
 
-# Extract the headers and clean
-washed_headers = [header.get_text(strip=True) for header in cr_table_washed.find('tr')]
-clean_washed_headers = [header for header in washed_headers if header != '']
-# print(clean_washed_headers)
+# # Extract the headers and clean
+# washed_headers = [header.get_text(strip=True) for header in cr_table_washed.find('tr')]
+# clean_washed_headers = [header for header in washed_headers if header != '']
 
-for i in range(0, len(clean_washed_headers)):
-    costa_rica_dictionary[clean_washed_headers[i]] = []
+# # add headers to dictionary
+# for i in range(0, len(clean_washed_headers)):
+#     costa_rica_dictionary[clean_washed_headers[i]] = []
 
-for row in cr_table_washed.find_all('tr')[1:]:
-    cells = row.find_all('td')
-    for i, cell in enumerate(cells):
-        cell_data = cell.find('div', class_='mtr-cell-content').get_text(strip=True)
-        costa_rica_dictionary[clean_washed_headers[i]].append(cell_data)
+def headers_for_dictionary(soup_table, dictionary):
+    headers = [header.get_text(strip=True) for header in soup_table.find('tr')]
+    clean = [header for header in headers if header != '']
+    for i in range(0, len(clean)):
+        dictionary[clean[i]] = []
+    return clean
+
+
+# Extract data and add to dictionary
+# for row in cr_table_washed.find_all('tr')[1:]:
+#     cells = row.find_all('td')
+#     for i, cell in enumerate(cells):
+#         cell_data = cell.find('div', class_='mtr-cell-content').get_text(strip=True)
+#         costa_rica_dictionary[clean_washed_headers[i]].append(cell_data)
+
+def extract_data_from_table(soup_table, headers, dictionary):
+    for row in soup_table.find_all('tr')[1:]:
+        cells = row.find_all('td')
+        for i, cell in enumerate(cells):
+            cell_data = cell.find('div', class_='mtr-cell-content').get_text(strip=True)
+            dictionary[headers[i]].append(cell_data)
+    return
+
+washed_table_headers = headers_for_dictionary(cr_table_washed, costa_rica_dictionary)
+extract_data_from_table(cr_table_washed, washed_table_headers, costa_rica_dictionary)
 
 
 print(costa_rica_dictionary)
